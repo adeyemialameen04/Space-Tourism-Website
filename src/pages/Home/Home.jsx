d6dd9c24484e5b62fc1ac883e8ca43e7dcc55583
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-07-12 16:43:01 -0500

    finished creating the homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,27 @@
 import './home.css';
+import Navbar from "../../components/Navbar";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home__section" >
+      <Navbar />
+      <div className="container-section home__container">
+        <div className="home__info">
+          <h1>So you want to travel to</h1><br />
+          <span>Space</span>
+          <p>
+            Let's face it; if you want to go to space, you might
+            as well genuinely go to outer space and not hover
+            kind of on the edge of it. Well sit back, and relax
+            because well give you a truly out of this world
+            experience!
+          </p>
+        </div>
+        <div className="explore">
+          <div>Explore</div>
+        </div>
+      </div>
+    </section>
   );
 };
 
