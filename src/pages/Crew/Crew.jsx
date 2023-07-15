c977f8950d2d78b6baa5b42f966861928301f6ff
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-07-15 02:37:02 -0500

    created the cre page

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,8 +1,45 @@
 import './crew.css';
+import { crew } from "../../data/crew";
+import Navbar from "../../components/Navbar";
+import { useState } from "react";
+import { BsRecord, BsRecordFill } from "react-icons/bs";
 
 const Crew = () => {
+
+  const [value, setValue] = useState(0);
+
+  const { name, images, role, bio } = crew[value];
   return (
-    <div>Crew</div>
+    <section className="crew__section" >
+      <Navbar />
+      <h1 className="title"><span>02</span> Meet Your Crew</h1>
+      <div className="crew__container">
+        <div className="crew__img">
+          <img src={images.webp} alt="" />
+        </div>
+        <div className="crew__info">
+          <div className="circles__btn">
+            {
+              crew.map((item, index) => (
+                <i
+                  key={item.id}
+                  className={`circles ${index === value && 'active-btn'}`}
+                  onClick={() => setValue(index)}>
+                  {index === value ? <BsRecordFill /> : <BsRecord />}
+                </i>
+              ))
+            }
+          </div>
+          {
+            <article className="crew">
+              <small>{role}</small>
+              <h4>{name}</h4>
+              <p>{bio}</p>
+            </article>
+          }
+        </div>
+      </div>
+    </section>
   );
 };
 
